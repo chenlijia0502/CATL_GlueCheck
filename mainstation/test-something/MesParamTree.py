86d7b91d497b72350bcbebb3016103f470b931a3
HUANG YUEHONG <502505040@qq.com>
2022-01-04 21:04:43 +0800

    MES已在车间测试过

--- mainstation/test-something/MesParamTree.py
+++ mainstation/test-something/MesParamTree.py
@@ -22,18 +22,23 @@ class MesParamTreeWidget(QtWidgets.QDialog):
         self._initmes()
         self.pushButton.clicked.connect(self.sendmes)
         self.ptbNotSend.clicked.connect(self.notSend)
+        self.pushButton_chuzhan.clicked.connect(self.chuzhan)
+        self.pushButton_jinzhan.clicked.connect(self.jinzhan)
         self.setWindowModality(Qt.ApplicationModal)
         self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint)
 
     def _initmes(self):
         try:
             dictconfig = readconfig()
+            #print (dictconfig)
+            print(dictconfig["MES"]['username'],dictconfig["MES"]['password'], dictconfig["MES"]['url'])
             t = HttpAuthenticated(username=dictconfig["MES"]['username'], password=dictconfig["MES"]['password'])
             self.client = Client(dictconfig["MES"]['url'], transport=t)
             self.machineIntegrationParametricData = []
-        except:
+        except Exception as e:
             s_msg = " MES error "
-            getqueue_queue_kxlog().put(('mes', logging.ERROR, s_msg))
+            print (s_msg, e)
+            #getqueue_queue_kxlog().put(('mes', logging.ERROR, s_msg))
 
     def _initui(self):
         self.setMinimumWidth(600)
@@ -73,31 +78,40 @@ class MesParamTreeWidget(QtWidgets.QDialog):
         self.horizontalLayout_13.addWidget(self.ptbNotSend)
         self.horizontalLayout_13.addStretch()
 
+        self.pushButton_jinzhan = QtWidgets.QPushButton()
+        self.pushButton_jinzhan.setText("进站")
+        self.pushButton_chuzhan = QtWidgets.QPushButton(self)
+        self.pushButton_chuzhan.setText("出站")
+
         # 卷针数据添加
-        self.widget_juanzhen = QtWidgets.QWidget(self)
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget_juanzhen)
-        self.label = QtWidgets.QLabel(self.widget_juanzhen)
-        self.label.setStyleSheet("font: 14pt \"Arial Rounded MT Bold\";\n"
-                                 "border:1px solid rgb(0, 0, 0);")
-        self.label.setText("卷针号(JZBH)")
-        self.horizontalLayout.addWidget(self.label)
-        self.juanzhencombox = QtWidgets.QComboBox(self.widget_juanzhen)
-        self.juanzhencombox.addItem("1")
-        self.juanzhencombox.addItem("2")
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.juanzhencombox.sizePolicy().hasHeightForWidth())
-        self.juanzhencombox.setSizePolicy(sizePolicy)
-        self.juanzhencombox.setStyleSheet("font: 18pt \"Arial\";")
-        self.horizontalLayout.addWidget(self.juanzhencombox)
-        self.horizontalLayout.setStretch(0, 1)
-        self.horizontalLayout.setStretch(1, 1)
-        self.verlayout.addWidget(self.widget_juanzhen)
-        self.verlayout.addWidget(self.widget_13)
+        # self.widget_juanzhen = QtWidgets.QWidget(self)
+        # self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget_juanzhen)
+        # self.label = QtWidgets.QLabel(self.widget_juanzhen)
+        # self.label.setStyleSheet("font: 14pt \"Arial Rounded MT Bold\";\n"
+        #                          "border:1px solid rgb(0, 0, 0);")
+        # self.label.setText("卷针号(JZBH)")
+        # self.horizontalLayout.addWidget(self.label)
+        # self.juanzhencombox = QtWidgets.QComboBox(self.widget_juanzhen)
+        # self.juanzhencombox.addItem("1")
+        # self.juanzhencombox.addItem("2")
+        # sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        # sizePolicy.setHorizontalStretch(0)
+        # sizePolicy.setVerticalStretch(0)
+        # sizePolicy.setHeightForWidth(self.juanzhencombox.sizePolicy().hasHeightForWidth())
+        # self.juanzhencombox.setSizePolicy(sizePolicy)
+        # self.juanzhencombox.setStyleSheet("font: 18pt \"Arial\";")
+        # self.horizontalLayout.addWidget(self.juanzhencombox)
+        # self.horizontalLayout.setStretch(0, 1)
+        # self.horizontalLayout.setStretch(1, 1)
+        # self.verlayout.addWidget(self.widget_juanzhen)
+        #self.verlayout.addWidget(self.widget_13)
+        #self.verlayout.addWidget(self.pushButton)
+        #self.verlayout.addWidget(self.pushButton_jinzhan)
+        self.verlayout.addWidget(self.pushButton_chuzhan)
         self.verlayout.setStretch(0, 16)
+
         self.verlayout.setStretch(1, 1)
-        self.verlayout.setStretch(2, 2)
+        #self.verlayout.setStretch(2, 1)
 
     def _initparam(self):
         params = []
@@ -109,20 +123,20 @@ class MesParamTreeWidget(QtWidgets.QDialog):
             if isinstance(self.dict_param[key]['value'], str) or valuedata is None:
                 stype = "str"
                 value = self.dict_param[key]['value']
-                dict_single = {'name': name, 'type': stype, 'isvisible': isvisible, 'readonly': ischangeable,
+                dict_single = {'name': name, 'type': stype, 'visible': isvisible, 'readonly': ischangeable,
                                'value': value}
             else:
                 stype = "list"
                 value = {}
                 for v in valuedata:
                     value[v] = valuedata[v]
-                dict_single = {'name': name, 'type': stype, 'isvisible': isvisible, 'readonly': ischangeable,
+                dict_single = {'name': name, 'type': stype, 'visible': isvisible, 'readonly': ischangeable,
                                'values': value}
             params.append(dict_single)
         self.p = KxParameter.create(name='params', type='group', children=params)
         self.h_parameterTree.setParameters(self.p, showTop=False)
-        self.p.param("当前电芯号(sfc)").setWritable(True)
-        self.p.param("当前电芯号(sfc)").setFocus()
+
+
 
     def getbool(self, svalue):
         if svalue.lower() == "false" or svalue == "0":
@@ -130,29 +144,6 @@ class MesParamTreeWidget(QtWidgets.QDialog):
         else:
             return True
 
-    def sendmes(self):
-        try:
-            self.machineIntegrationParametricData[0].name = "JZBH"
-            self.machineIntegrationParametricData[0].value = self.juanzhencombox.currentText()
-            self.machineIntegrationParametricData[0].dataType = "NUMBER"
-            dict_senddata = {}
-            for key in self.dict_param:
-                name = self.dict_param[key]['chinesename'] + '(' + key + ")"
-                value = self.p.param(name).value()
-                dict_senddata[key] = value
-            dict_senddata["parametricDataArray"] = self.machineIntegrationParametricData
-            payloads = {"resourceRequest": [dict_senddata]}
-            result = self.client.service.dataCollectForResourceFAI(**payloads)
-            if result[0] == 0:
-                warnwindows = QtWidgets.QMessageBox()
-                respond = warnwindows.information(self, "上传成功", "数据已成功上传", QtWidgets.QMessageBox.Ok)
-                self.close()
-            else:
-                errorwindow = QtWidgets.QMessageBox()
-                respond = errorwindow.warning(self, "警告，数据上传失败", result[1], QtWidgets.QMessageBox.Ok)
-        except:
-            s_msg = " MES SEND ERROR "
-            getqueue_queue_kxlog().put(('mes', logging.ERROR, s_msg))
 
     def setdata(self, list_tuple_data):
         try:
@@ -210,13 +201,119 @@ class MesParamTreeWidget(QtWidgets.QDialog):
         self.save_measure_result()
         self.close()
 
-    def show(self):
-        super().show()
-        self.p.param('当前电芯号(sfc)').setValue("")
-        self.focusOnSFC()
 
-    def focusOnSFC(self):
-        self.p.param('当前电芯号(sfc)').setFocus()
+
+
+
+            # getqueue_queue_kxlog().put(('mes', logging.ERROR, s_msg))
+
+
+
+    def jinzhan(self):
+        print("进站命令")
+        try:
+            # TODO 下面几句话传输结果，将结果按照格式放入self.machineIntegrationParametricData
+            self.machineIntegrationParametricData = []
+            # self.machineIntegrationParametricData.append(
+            #     self.client.factory.create('tns:machineIntegrationParametricData'))
+            # self.machineIntegrationParametricData[0].name = "JZBH"
+            # self.machineIntegrationParametricData[0].value = self.juanzhencombox.currentText()
+            # self.machineIntegrationParametricData[0].dataType = datatype
+            #
+            dict_senddata = {}
+            print(self.client.service)
+            for key in self.dict_param:
+                name = self.dict_param[key]['chinesename'] + '(' + key + ")"
+                value = self.p.param(name).value()
+                dict_senddata[key] = value
+            dict_senddata["parametricDataArray"] = self.machineIntegrationParametricData
+            payloads = {"resourceRequest": [dict_senddata]}
+            print(payloads)
+            result = self.client.service.dataCollectForSfcEX(**payloads)  # 出站api
+            print('上传回复结果：', result)
+            if result[0] == 0:
+                warnwindows = QtWidgets.QMessageBox()
+                respond = warnwindows.information(self, "上传成功", "数据已成功上传", QtWidgets.QMessageBox.Ok)
+                self.close()
+            else:
+                errorwindow = QtWidgets.QMessageBox()
+                respond = errorwindow.warning(self, "警告，数据上传失败", result[1], QtWidgets.QMessageBox.Ok)
+        except:
+            s_msg = " MES SEND ERROR "
+            print(s_msg)
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+
+
+    def sendmes(self):
+        try:
+            # self.machineIntegrationParametricData[0].name = "JZBH"
+            # self.machineIntegrationParametricData[0].value = self.juanzhencombox.currentText()
+            # self.machineIntegrationParametricData[0].dataType = "NUMBER"
+            dict_senddata = {}
+            for key in self.dict_param:
+                name = self.dict_param[key]['chinesename'] + '(' + key + ")"
+                value = self.p.param(name).value()
+                dict_senddata[key] = value
+            dict_senddata["parametricDataArray"] = self.machineIntegrationParametricData
+            payloads = {"resourceRequest": [dict_senddata]}
+            result = self.client.service.dataCollectForResourceFAI(**payloads)
+            print ('上传回复结果：', result)
+            if result[0] == 0:
+                warnwindows = QtWidgets.QMessageBox()
+                respond = warnwindows.information(self, "上传成功", "数据已成功上传", QtWidgets.QMessageBox.Ok)
+                self.close()
+            else:
+                errorwindow = QtWidgets.QMessageBox()
+                respond = errorwindow.warning(self, "警告，数据上传失败", result[1], QtWidgets.QMessageBox.Ok)
+        except Exception as e:
+            s_msg = " MES SEND ERROR "
+            print(s_msg, e)
+            #getqueue_queue_kxlog().put(('mes', logging.ERROR, s_msg))
+
+
+
+
+    def chuzhan(self):
+        #self.sendmes()
+        print('出站命令')
+        try:
+            # TODO 下面几句话传输结果，将结果按照格式放入self.machineIntegrationParametricData
+            self.machineIntegrationParametricData = []
+            self.machineIntegrationParametricData.append(
+                self.client.factory.create('tns:machineIntegrationParametricData'))
+            self.machineIntegrationParametricData[0].name = "JSGJJCJG"
+            self.machineIntegrationParametricData[0].value = 1# 先上传结果是OK的情况
+            self.machineIntegrationParametricData[0].dataType = "NUMBER"
+
+            dict_senddata = {}
+            for key in self.dict_param:
+                name = self.dict_param[key]['chinesename'] + '(' + key + ")"
+                value = self.p.param(name).value()
+                dict_senddata[key] = value
+            dict_senddata["parametricDataArray"] = self.machineIntegrationParametricData
+
+            # TODO 需不需要"resourceRequest"修饰待定
+            payloads = dict_senddata
+
+            print(payloads)
+
+            result = self.client.service.dataCollectForSfcEx(payloads)  # 出站api
+
+            print('上传回复结果：', result)
+            if result[0] == 0:
+                warnwindows = QtWidgets.QMessageBox()
+                respond = warnwindows.information(self, "上传成功", "数据已成功上传", QtWidgets.QMessageBox.Ok)
+                self.close()
+            else:
+                errorwindow = QtWidgets.QMessageBox()
+                respond = errorwindow.warning(self, "警告，数据上传失败", result[1], QtWidgets.QMessageBox.Ok)
+        except Exception as e:
+            s_msg = " MES SEND ERROR "
+            print(s_msg, e)
+
 
 
 if __name__ == "__main__":
